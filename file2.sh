9a836f3f9e469c85627f658593837a3d2d71875b
kreshna1 <pavan.bejjarapu@gmail.com>
2024-07-06 23:42:06 +0530

    hhh

--- file2.sh
+++ file2.sh
@@ -1,13 +1,25 @@
 #!bin/bash
-USERID = $(id -u)
-if [ $USERID -ne 0]:
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+
 then
+
     echo "error in instalation"
+
 fi
+
 yum install mysql -y
-if [ $id -ne 0]
+
+if [ $? -ne 0]
+
 then
+
     echo "instalation failure"
+
     exit 1
+
 else
+
     echo "success instalation"
